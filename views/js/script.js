c6d859f5e81618b5f17babb38cd849279df83c74
Agni Sain <agnisain123@gmail.com>
2021-04-12 23:00:19 +0530

    Added Dark Mode

--- views/js/script.js
+++ views/js/script.js
@@ -117,3 +117,19 @@ $('.js--wp-6').waypoint(
     offset: '80%',
   }
 );
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
